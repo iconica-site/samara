8f70efa5cbe7f460aed62fe35fd4608122747ebb
Eugene <yousucker8@gmail.com>
2024-12-25 23:55:05 +0300

    v8.6

--- assets/js/siderbar.js
+++ assets/js/siderbar.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <a href=""><li>Свидетельство о регистрации</li></a>
         <a href="../vakansii/vakansii.html"><li>Вакансии</li></a>
         <a href="../Politika_v_otnoshenii_obrabotki_personal_nyh_dannyh/Politika_v_otnoshenii_obrabotki_personal_nyh_dannyh.html"><li>Политика конфиденциальности</li></a>
-        <a href=""><li>Наши достижения</li></a>
+        <a href="../Nashi_dostizheniya/Nashi_dostizheniya.html"><li>Наши достижения</li></a>
         <a href="../contacts/contacts.html"><li>Контакты</li></a>
         <a href=""><li>Приказ об организации платных услуг</li></a>
         <a href="../Litsenzii__sertifikaty__svidetel_stva__Ustav/Litsenzii__sertifikaty__svidetel_stva__Ustav.html"> <li>Лицензии, сертификаты, свидетельства, Устав</li></a>
@@ -41,6 +41,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (document.querySelector('h1').textContent.trim() === 'Политика конфиденциальности') {
         document.querySelector('ul a:nth-child(5) li').classList.add('active');
+    }
+
+    if (document.querySelector('h1').textContent.trim() === 'Наши достижения') {
+        document.querySelector('ul a:nth-child(6) li').classList.add('active');
     }
 
     if (document.querySelector('h1').textContent.trim() === 'Контакты') {
