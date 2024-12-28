aa41915e7b0ec3dad88b76aecc1ff932472649b8
Eugene <yousucker8@gmail.com>
2024-12-28 11:52:08 +0300

    v9.9.9

--- assets/js/header.js
+++ assets/js/header.js
@@ -356,7 +356,7 @@ header.innerHTML = `
     </form>
 </div>
 
-        <div class="call-button" id="callButton" style="position: fixed; right: 20px; top: 20px; z-index: 9999999; cursor: pointer;">
+        <div class="call-button" id="callButton" style="position: fixed; right: 20px; bottom: 20px; z-index: 9999999; cursor: pointer;">
             <img style="width: 100px;" src="../assets/img/call.png" alt="">
         </div>
 
