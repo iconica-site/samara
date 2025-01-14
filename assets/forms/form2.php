1bceff8af49afd230e5e27c580f20d7d7adf2b16
eugeneplug <68537036+eugeneplug@users.noreply.github.com>
2025-01-14 11:26:27 +0300

    v

--- assets/forms/form2.php
+++ assets/forms/form2.php
@@ -1,31 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Очистка и получение данных из формы
     $name = htmlspecialchars($_POST['name']);
     $phone = htmlspecialchars($_POST['phone']);
     $email = htmlspecialchars($_POST['email']);
 
-    // Адрес получателя
     $to = "gp6samara@yandex.ru";
-
-    // Тема письма
     $subject = "Новая заявка с сайта";
+    $message = "Имя: $name\nТелефон: $phone\nВопрос: $email";
+    $headers = "From: no-reply@yourwebsite.com\r\n" .
+               "Reply-To: $phone\r\n" .
+               "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Сообщение
-    $message = "Имя: $name\n";
-    $message .= "Телефон: $phone\n";
-    $message .= "Почта: $email\n";
-
-    // Заголовки
-    $headers = "From: no-reply@yourwebsite.com\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    // Отправка письма
     if (mail($to, $subject, $message, $headers)) {
-        echo "<p style='color: green; text-align: center;'>Спасибо за ваше сообщение! Мы свяжемся с вами скоро.</p>";
+        echo "Спасибо за ваше сообщение! Мы свяжемся с вами скоро.";
     } else {
-        echo "<p style='color: red; text-align: center;'>Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте снова.</p>";
+        echo "Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте снова.";
     }
 }
 ?>
